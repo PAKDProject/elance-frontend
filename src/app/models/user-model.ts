d1f9ae5c9dbd86469cff41bf9b768db4c653b7ee
S00170565 <S00170565@mail.itsligo.ie>
2018-10-15 21:17:24 +0100

    Started making the user interface

---
+++ src/app/models/user-model.ts
@@ -0,0 +1,36 @@
+export interface IUser {
+  userID: number;
+  email: string;
+  fName?: string;
+  lName?: string;
+  dob?: Date;
+  summary?: string;
+  skills?: ISkill[];
+  educationItems?: IEducationItem[];
+  avatarUrl?: string;
+  backgroundUrl?: string;
+  socialLinks?: ISocialLink[];
+  tagline?: string;
+  contacts?: IUser[];
+}
+
+export interface ISocialLink {
+  socialPlatformName?: string;
+  linkUrl: string;
+}
+
+export interface IEducationItem {
+  degreeTitle?: string;
+  startYear?: string;
+  endYear?: string;
+  collegeName?: string;
+  grade?: string;
+  description?: string;
+}
+
+export interface ISkill {
+  title?: string;
+  description?: string;
+  // An idea to be discussed. 1-10 on progress bar or something
+  levelOfConfidence?: number;
+}
