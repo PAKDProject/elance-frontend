5e18fe777546cb72f235e7424395a2cea5ab0e71
S00170565 <S00170565@mail.itsligo.ie>
2018-10-25 12:11:33 +0100

    no bugs that i can find here

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -14,20 +14,11 @@ import * as AWS from 'aws-sdk';
 })
 export class RegisterFormComponent implements OnInit {
   isLinear = true;
-<<<<<<< HEAD
   skills: string[] = []
 
   image: any;
   fileToUpload: File;
   constructor() { }
-=======
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  firstName: string
-  lastName: string 
-
-  constructor(private _formBuilder: FormBuilder, private _userStore: TempUserStorageService, private _router: Router) { }
->>>>>>> f568524503ab6cfbf511aaf05d38db5365e57a59
 
   ngOnInit() {
   }
@@ -47,7 +38,7 @@ export class RegisterFormComponent implements OnInit {
     }
   }
 
-  addUser(fName: string, lName: string){
+  addUser(fName: string, lName: string) {
     let user: IUser = {
       userID: 0,
       email: localStorage.getItem("email"),
@@ -55,7 +46,6 @@ export class RegisterFormComponent implements OnInit {
       lName
     }
 
-    this._userStore.setUser(user)
-    this._router.navigate(['/user-dashboard'])
+
   }
 }
