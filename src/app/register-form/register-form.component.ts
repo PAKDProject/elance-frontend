88629c3320163feca03ab84b42ca22b8bb108259
S00170565 <S00170565@mail.itsligo.ie>
2018-10-28 13:20:32 +0000

    validation continues

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IUser } from '../models/user-model';
+import { IUser, ISkill } from '../models/user-model';
 import { TempUserStorageService } from '../temp-user-storage.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
@@ -14,11 +14,13 @@ import * as AWS from 'aws-sdk';
 })
 export class RegisterFormComponent implements OnInit {
   isLinear = true;
-  skills: string[] = []
-  user: IUser = { email: "sample@gmail.com", userID: 1 };
+  skillsAdded: ISkill[] = []
+  user: IUser = { email: "sample@gmail.com", userID: 1, skills: this.skillsAdded };
 
 
   personalDetailsForm: FormGroup;
+  aboutYouForm: FormGroup;
+  skillForm: FormGroup;
 
   constructor(private fb: FormBuilder) { }
 
@@ -28,7 +30,17 @@ export class RegisterFormComponent implements OnInit {
       fName: [this.user.fName, Validators.required],
       lName: [this.user.lName, Validators.required],
       dob: [this.user.dob, [Validators.min(1900), Validators.max(2018)]],
-      phone: [this.user.phone, [Validators.pattern("\\d{6,8}")]]
+      phone: [this.user.phone, [Validators.pattern("\\d{6,10}")]]
+    });
+
+    this.aboutYouForm = this.fb.group({
+      tagline: '',
+      description: ['', [Validators.maxLength(300)]]
+    });
+
+    this.skillForm = this.fb.group({
+      skillTitle: '',
+      skillDescription: ''
     });
 
     this.personalDetailsForm.valueChanges.subscribe(data => {
@@ -38,23 +50,33 @@ export class RegisterFormComponent implements OnInit {
       this.user.phone = data.phone;
       console.log(this.user);
     });
+
+    this.aboutYouForm.valueChanges.subscribe(data => {
+      this.user.tagline = data.tagline;
+      this.user.summary = data.description;
+      console.log(this.user);
+    });
+
+
   }
 
-
-
-  addItem(title: string) {
-    this.skills.push(title);
-  }
-  removeItem(skill: string) {
-    let index = this.skills.indexOf(skill);
-    if (index === -1) {
-      console.log("error");
-
+  addSkill() {
+    let alreadyContained = false;
+    this.skillsAdded.forEach(skill => {
+      if (skill.title.toLowerCase() === this.skillForm.get('skillTitle').value.toLowerCase()) {
+        alreadyContained = true
+      }
+    });
+    if (!alreadyContained) {
+      this.skillsAdded.push({
+        title: this.skillForm.get('skillTitle').value,
+        description: this.skillForm.get('skillDescription').value
+      });
     }
-    else {
-      this.skills.splice(index, 1);
-    }
+    this.skillForm.reset();
   }
+
+
 
   addUser(fName: string, lName: string) {
     let user: IUser = {
