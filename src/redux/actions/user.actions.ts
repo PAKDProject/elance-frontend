725bc44cacf279446b1fb6cb151b1d95076bfee2
s00168420 <s00168420@mail.itsligo.ie>
2018-10-30 20:56:25 +0000

    Reduxified frontend

---
+++ src/redux/actions/user.actions.ts
@@ -0,0 +1,13 @@
+import { IUser } from "src/app/models/user-model";
+
+export class RequestUserSuccessAction {
+    static readonly type = '[User] Request Success'
+
+    constructor(public payload: IUser) { }
+}
+
+export class RequestUserFailedActions {
+    static readonly type = '[User] Request Fail'
+
+    constructor(public errorMessage: string) { }
+}
