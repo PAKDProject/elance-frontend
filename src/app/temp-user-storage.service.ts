320b7bec9a21c80ecbd0be18469ebcf89b8c8ab1
s00168420 <s00168420@mail.itsligo.ie>
2018-10-30 20:59:38 +0000

    Changed getTestUser to return an observable as though it were a http req

--- src/app/temp-user-storage.service.ts
+++ src/app/temp-user-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { IUser, IEducationItem, ISkill, ISocialLink } from './models/user-model';
 import { getLocaleDateTimeFormat } from '@angular/common';
 import { ConvertActionBindingResult } from '@angular/compiler/src/compiler_util/expression_converter';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +12,17 @@ export class TempUserStorageService {
   constructor() { }
   private user: IUser
 
-  getUser() {
-    return this.user;
-  }
-
   setUser(value) {
     this.user = value;
   }
-  
-  getTestUser() {
+
+  getTestUser(): Observable<IUser> {
     this.user = {
       userID: 1,
       email: 'stocksteve@email.com',
       fName: 'Stock',
       lName: 'Steve',
-      dob: new Date(19/1/1999),
+      dob: new Date(19 / 1 / 1999),
       summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non eros viverra, fringilla massa rhoncus, dictum nulla. In rhoncus nunc sed ante maximus tincidunt. Praesent ut lectus nisl. Etiam elementum dictum elit at aliquet. Sed vitae vehicula massa, sed luctus velit. Morbi aliquam sodales tempor. Praesent tempus, felis et luctus porttitor, quam lectus elementum nulla, sit amet pretium nisi dolor vel eros. Nulla vestibulum eu velit ac semper. Morbi gravida rhoncus libero, a rhoncus sapien. Morbi non sem a ligula viverra fermentum elementum ac odio. Fusce eget justo eros.',
       educationItems: [
         {
@@ -94,7 +91,6 @@ export class TempUserStorageService {
       contacts: []
     }
 
-    return this.user;
+    return of(this.user)
   }
-  
 }
