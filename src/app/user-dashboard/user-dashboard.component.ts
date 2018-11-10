92b88f4707a97c6c96ff30b3a9927c876b7750e9
Daire Finn <daire.finn@mail.itsligo.ie>
2018-11-10 16:44:28 +0000

    Carousel buttons now work and cool sweeet

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IJob } from 'src/models/job-model';
 import { Observable } from 'rxjs';
 import { Select, Store } from '@ngxs/store';
@@ -6,6 +6,7 @@ import { RequestJobs } from 'src/redux/actions/job.actions';
 import { JobsState } from 'src/redux/states/job.state';
 import { MatDialog } from "@angular/material";
 import { CreateJobModalComponent } from "../modals/create-job-modal/create-job-modal.component";
+import { DragScrollComponent } from 'ngx-drag-scroll/lib';
 
 @Component({
   selector: "app-user-dashboard",
@@ -15,6 +16,40 @@ import { CreateJobModalComponent } from "../modals/create-job-modal/create-job-m
 export class UserDashboardComponent implements OnInit {
 
   jobs: IJob[];
+
+  //Carousels
+  @ViewChild('activeJobs', {read: DragScrollComponent}) activeCarousel: DragScrollComponent;
+  @ViewChild('inactiveJobs', {read: DragScrollComponent}) inactiveCarousel: DragScrollComponent;
+  @ViewChild('contacts', {read: DragScrollComponent}) contactsCarousel: DragScrollComponent;
+  
+  moveCarousel(direction: string, carousel: number) {
+    if(direction == 'left') {
+      switch(carousel) {
+        case 1:
+          this.activeCarousel.moveLeft();
+          break;
+        case 2:
+          this.inactiveCarousel.moveLeft();
+          break;
+        case 3:
+          this.contactsCarousel.moveLeft();
+          break;
+      }
+    }
+    else {
+      switch(carousel) {
+        case 1:
+          this.activeCarousel.moveRight();
+          break;
+        case 2:
+          this.inactiveCarousel.moveRight();
+          break;
+        case 3:
+          this.contactsCarousel.moveRight();
+          break;
+      }
+    }
+  }
   
   @Select(JobsState.getJobs)
   jobs$: Observable<IJob[]>;
