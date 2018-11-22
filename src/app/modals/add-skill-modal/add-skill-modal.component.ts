1be9a8d4f074e3e90f7773607e80024410331aa1
S00170565 <s00170565@mail.itsligo.ie>
2018-11-22 13:16:17 +0000

    continued typeahead skill box

--- src/app/modals/add-skill-modal/add-skill-modal.component.ts
+++ src/app/modals/add-skill-modal/add-skill-modal.component.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  FormsModule,
-  FormBuilder
-} from "@angular/forms";
-import { NgSelectModule, NgOption } from "@ng-select/ng-select";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { ISkills } from "../../../models/skill-model";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-add-skill-modal",
@@ -16,51 +11,38 @@ import { NgSelectModule, NgOption } from "@ng-select/ng-select";
 })
 export class AddSkillModalComponent implements OnInit {
   skillForm: FormGroup;
-  skillLevel: string = "Novice";
+  skills: ISkills[];
+  skillsLoading: boolean;
 
-  skills = [
-    { id: "1", skill: "JavaScript", area: "Web Programming" },
-    { id: "4", skill: "TypeScript", area: "Web Programming" },
-    { id: "5", skill: "Angular", area: "Web Programming" },
-    { id: "6", skill: "C#", area: "Old School" },
-    { id: "7", skill: "Java", area: "Old School" },
-    { id: "8", skill: "Asp.Net Mvc", area: "Old School" }
-  ];
+  selectedSkills: ISkills[] = [];
+  addCustomSkill = term => ({
+    id: term.toLocaleLowerCase(),
+    skillTitle: term,
+    category: "Custom"
+  });
 
-  refactoredSkills = [];
-
-  selectedSkill: any;
-  addCustomSkill = term => ({ id: term, skill: term, area: "Custom" });
-
-  constructor(private _fb: FormBuilder) {}
+  constructor(private _fb: FormBuilder, private _http: HttpClient) {}
 
   ngOnInit() {
-    this.skills.forEach(skill => {
-      let noviceSkill = {
-        id: `novice${skill.skill}`,
-        skill: `${skill.skill} - Novice`,
-        area: skill.area,
-        skillType: skill.skill
-      };
-      let intermediateSkill = {
-        id: `intermediate${skill.skill}`,
-        skill: `${skill.skill} - Intermediate`,
-        area: skill.area,
-        skillType: skill.skill
-      };
-      let expertSkill = {
-        id: `expert${skill.skill}`,
-        skill: `${skill.skill} - Expert`,
-        area: skill.area,
-        skillType: skill.skill
-      };
-
-      this.refactoredSkills.push(noviceSkill);
-      this.refactoredSkills.push(intermediateSkill);
-      this.refactoredSkills.push(expertSkill);
+    this.getSkills().subscribe(res => {
+      console.log(res);
+      this.skillsLoading = false;
+      this.skills = res;
     });
+
     this.skillForm = this._fb.group({
       selectedSkill: []
     });
+  }
+
+  getSkills(): Observable<ISkills[]> {
+    return this._http.get<ISkills[]>("assets/data/skills.json");
+  }
+  customSearchFn(term: string, item: ISkills) {
+    term = term.toLocaleLowerCase();
+    return (
+      item.skillTitle.toLocaleLowerCase().indexOf(term) > -1 ||
+      item.category.toLocaleLowerCase() === term
+    );
   }
 }
