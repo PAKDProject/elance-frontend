1bb836352a4908a08afc4915fefab5f115df3aba
Daire Finn <daire.finn@mail.itsligo.ie>
2019-02-20 20:49:13 +0000

    fuccin cards

--- src/app/fucc-jobs/fucc-jobs.component.ts
+++ src/app/fucc-jobs/fucc-jobs.component.ts
@@ -12,9 +12,9 @@ export class FuccJobsComponent implements OnInit {
   @Output() dismissFormEmit: EventEmitter<boolean> = new EventEmitter<boolean>();
   fuccJob: IJob[] = [
     {
-      title: 'Job 1',
+      title: 'Develop Ancient Technology',
       employerID: '1',
-      employerName: 'John',
+      employerName: 'Microsoft',
       location: 'Dublin',
       remote: false,
       payment: 3000,
@@ -23,9 +23,9 @@ export class FuccJobsComponent implements OnInit {
       description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     },
     {
-      title: 'Job 2',
+      title: 'Angular Version 72',
       employerID: '2',
-      employerName: 'Jeff',
+      employerName: 'Google',
       location: 'Galway',
       remote: false,
       payment: 4000,
@@ -34,9 +34,9 @@ export class FuccJobsComponent implements OnInit {
       description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     },
     {
-      title: 'Job 3',
+      title: 'Virgin Sacrifice for Lord Jeff',
       employerID: '3',
-      employerName: 'Mike',
+      employerName: 'Amazon',
       remote: true,
       payment: 3500,
       datePosted: new Date(),
