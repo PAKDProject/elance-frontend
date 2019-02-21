1343bdad5b72ccbe73a4a2b17429881e201fd27d
Daire Finn <daire.finn@mail.itsligo.ie>
2019-02-21 12:44:36 +0000

    Messages stuffs

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -36,69 +36,69 @@ export class MessagesComponent implements OnInit {
   }
 
   //TEMP
-  sampleConversation: IMessage[] = [
-    {
-      content: 'Hello',
-      isSeen: true,
-      recipientId: 'user1',
-      senderId: 'user2',
-      timestamp: new Date()
-    },
-    {
-      content: 'Hi',
-      isSeen: true,
-      recipientId: 'user2',
-      senderId: 'user1',
-      timestamp: new Date()
-    },
-    {
-      content: 'Who are you?',
-      isSeen: true,
-      recipientId: 'user1',
-      senderId: 'user2',
-      timestamp: new Date()
-    },
-    {
-      content: 'My name a Jeff',
-      isSeen: true,
-      recipientId: 'user2',
-      senderId: 'user1',
-      timestamp: new Date()
-    },
-    {
-      content: 'What is this place?',
-      isSeen: true,
-      recipientId: 'user1',
-      senderId: 'user2',
-      timestamp: new Date()
-    },
-    {
-      content: 'This is intellilance, the greatest job site ever created',
-      isSeen: true,
-      recipientId: 'user2',
-      senderId: 'user1',
-      timestamp: new Date()
-    },
-    {
-      content: "My God, It's beautiful",
-      isSeen: false,
-      recipientId: 'user1',
-      senderId: 'user2',
-      timestamp: new Date()
-    },
-    {
-      content: 'You have been chosen to be a part of an elite group of frelancers who can get jobs easily at the click of a button',
-      isSeen: true,
-      recipientId: 'user2',
-      senderId: 'user1',
-      timestamp: new Date()
-    },
-    {
-      content: "Cool beans",
-      isSeen: false,
-      recipientId: 'user1',
-      senderId: 'user2',
-      timestamp: new Date()
-    }
-  ]
+  sampleConversation: IMessage[] = []
+  //   {
+  //     content: 'Hello',
+  //     isSeen: true,
+  //     recipientId: 'user1',
+  //     senderId: 'user2',
+  //     timestamp: new Date()
+  //   },
+  //   {
+  //     content: 'Hi',
+  //     isSeen: true,
+  //     recipientId: 'user2',
+  //     senderId: 'user1',
+  //     timestamp: new Date()
+  //   },
+  //   {
+  //     content: 'Who are you?',
+  //     isSeen: true,
+  //     recipientId: 'user1',
+  //     senderId: 'user2',
+  //     timestamp: new Date()
+  //   },
+  //   {
+  //     content: 'My name a Jeff',
+  //     isSeen: true,
+  //     recipientId: 'user2',
+  //     senderId: 'user1',
+  //     timestamp: new Date()
+  //   },
+  //   {
+  //     content: 'What is this place?',
+  //     isSeen: true,
+  //     recipientId: 'user1',
+  //     senderId: 'user2',
+  //     timestamp: new Date()
+  //   },
+  //   {
+  //     content: 'This is intellilance, the greatest job site ever created',
+  //     isSeen: true,
+  //     recipientId: 'user2',
+  //     senderId: 'user1',
+  //     timestamp: new Date()
+  //   },
+  //   {
+  //     content: "My God, It's beautiful",
+  //     isSeen: false,
+  //     recipientId: 'user1',
+  //     senderId: 'user2',
+  //     timestamp: new Date()
+  //   },
+  //   {
+  //     content: 'You have been chosen to be a part of an elite group of frelancers who can get jobs easily at the click of a button',
+  //     isSeen: true,
+  //     recipientId: 'user2',
+  //     senderId: 'user1',
+  //     timestamp: new Date()
+  //   },
+  //   {
+  //     content: "Cool beans",
+  //     isSeen: false,
+  //     recipientId: 'user1',
+  //     senderId: 'user2',
+  //     timestamp: new Date()
+  //   }
+  // ]
 }
