653e1111f221e8f3e7fcddd0cffe6c1e5cbefcb3
Daire Finn <daire.finn@mail.itsligo.ie>
2019-03-14 17:41:10 +0000

    filtered your posted jobs from browse list

--- src/app/browse-jobs/grid/grid.component.ts
+++ src/app/browse-jobs/grid/grid.component.ts
@@ -1,16 +1,32 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { IJob } from 'src/models/job-model';
+import { UserState } from 'src/redux/states/user.state';
+import { IUser } from 'src/models/user-model';
+import { Observable } from 'rxjs';
+import { Select } from '@ngxs/store';
 @Component({
   selector: 'jobs-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
 export class GridComponent {
+  @Select(UserState.getUser)
+  user$: Observable<IUser>
 
   @Input('JobsInput') jobs: IJob[]
-  @Output() notify: EventEmitter<IJob> = new EventEmitter<IJob>(); 
+  filteredjobs: IJob[] = []
+  @Output() notify: EventEmitter<IJob> = new EventEmitter<IJob>()
 
   constructor() { }
+
+  ngOnInit() {
+    this.user$.subscribe(u => {
+      this.jobs.forEach(j => {
+        if(j.employerID != u.id)
+        { this.filteredjobs.push(j) }
+      })
+    }).unsubscribe()
+  }
 
   //Hides the job
   hideJob(j: IJob) {
--- src/app/browse-jobs/list/list.component.ts
+++ src/app/browse-jobs/list/list.component.ts
@@ -1,18 +1,34 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { IJob } from 'src/models/job-model';
+import { Select } from '@ngxs/store';
+import { UserState } from 'src/redux/states/user.state';
+import { Observable } from 'rxjs';
+import { IUser } from 'src/models/user-model';
 
 @Component({
   selector: 'jobs-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
+  @Select(UserState.getUser)
+  user$: Observable<IUser>
+  filteredjobs: IJob[] = []
 
   @Input('JobsInput')jobs: IJob[]
   @Output() notify: EventEmitter<IJob> = new EventEmitter<IJob>(); 
 
   constructor() { }
   
+  ngOnInit() {
+    this.user$.subscribe(u => {
+      this.jobs.forEach(j => {
+        if(j.employerID != u.id)
+        { this.filteredjobs.push(j) }
+      })
+    }).unsubscribe()
+  }
+
   //Hides the job
   hideJob(j: IJob) {
     this.notify.emit(j);
